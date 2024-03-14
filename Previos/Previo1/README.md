6452687ff8429c8dc3178210d42dd75c1359a1ec
Kevin Jiménez <kevinjimeneza21@gmail.com>
2024-03-13 20:32:29 -0600

    Ejemplo de como escribir archivos markdown y cambio en holaMundo.cpp
    Signed-off-by: Kevin Jiménez <kevinjimeneza21@gmail.com>

---
+++ Previos/Previo1/README.md
@@ -0,0 +1,3 @@
+# Previo1
+
+_En esta carpeta se encuentran todos los códigos utilizados en la primera sesión del curso Estructuras Abstractas de Bases y Algoritmos para Ingeniería_
--- Previos/Previo1/holaMundo.cpp
+++ Previos/Previo1/holaMundo.cpp
@@ -1,8 +1,9 @@
-// Ejemplo inicial de hola mundo
+// Anadir una linea para probar git stash
 
 #include <iostream>
 
 int main() {
-    std::cout << "Hola Mundo, estudiantes de IE0217!";
+    std::cout << "Hola a todos y a todas, estudiantes de IE0217!\n";
+    std::cout << "Probando el stash!";
     return 0;
 }
