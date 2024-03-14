4241f8dd3e12f978b81de3fadbd19711a2cb3b02
Kevin Jiménez <kevinjimeneza21@gmail.com>
2024-03-13 19:40:06 -0600

    Agregando mi primer programa en C++ (Tambien fue necesario agregar la carpeta de Previos y Previo1)
    Signed-off-by: Kevin Jiménez <kevinjimeneza21@gmail.com>

---
+++ Previos/Previo1/holaMundo.cpp
@@ -0,0 +1,8 @@
+// Ejemplo inicial de hola mundo
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo, estudiantes de IE0217!";
+    return 0;
+}
---
+++ Previos/Previo1/otroArchivo.cpp
@@ -0,0 +1,8 @@
+// Ejemplo de creacion de un segundo archivo
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo, desde otro archivo!";
+    return 0;
+}
