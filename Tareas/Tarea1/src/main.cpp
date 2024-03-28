db10226ac13caeceffe8c07980a8834f29318e61
kevin-jim21 <kevinjimeneza21@gmail.com>
2024-03-28 01:42:30 -0600

    Implementacion del ajuste de dificultad del juego
    Signed-off-by: kevin-jim21 <kevinjimeneza21@gmail.com>

--- Tareas/Tarea1/src/main.cpp
+++ Tareas/Tarea1/src/main.cpp
@@ -7,6 +7,20 @@
 
 int main() {
     int option;
+    int difficulty;
+    Ahorcado Struct;
+    Ahorcado *StructPointer;
+
+    // Asignar un puntero para la estructura
+    StructPointer = &Struct;
+
+    // Asignar dificultad facil por default (7 intentos)
+    StructPointer->maxAttemps = 7;
+
+    /* Dicionario de palabras predeterminado, se dejan espacios vacíos por si el usuario
+    desea añadir más palabras para jugar*/
+    std::string dictionary[30] = {"blanco", "bajo", "vaca", "verde", "helado", "preocupado",
+    "puerta", "dulce", "pollo", "nube", "estrella", "nervioso", "camino", "dibujo", "elefante"};
 
     /* Mostrar menu al usario utilizando bucle para mostrar el menu hasta
     que el usuario introduzca una opcion valida*/
@@ -23,6 +37,32 @@ int main() {
         // Acceder a cada una de las funcionalidades del juego
         switch(option) {
             case 1:
+                // Bucle para establecer la dificultad del juego
+                do {
+                    std::cout << "Selecciona una opción para establecer la dificultad del juego" << std::endl;
+                    std::cout << "1. Fácil (7 intentos permitidos)" << std::endl;
+                    std::cout << "2. Intermedio (5 intentos permitidos)" << std::endl;
+                    std::cout << "3. Difícil (3 intentos permitidos)" << std::endl;
+
+                    std::cin >> difficulty;
+
+                    // Ajustar la dificultad segun la escogencia del usuario
+                    switch(difficulty) {
+                        case 1:
+                            StructPointer->maxAttemps = 7;
+                            break;
+                        case 2:
+                            StructPointer->maxAttemps = 5;
+                            break;
+                        case 3:
+                            StructPointer->maxAttemps = 3;
+                            break;
+                        default:
+                            std::cout << "Esta opción no es válida. Por favor intenta de nuevo." << std::endl;;
+                            break;
+                    }
+
+                } while (difficulty < 1 || difficulty > 3);
 
                 break;
             case 2:
