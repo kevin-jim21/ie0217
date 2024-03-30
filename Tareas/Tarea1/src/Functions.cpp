38513a07575c64d26e497cf9403c1c141a44aaa6
kevin-jim21 <kevinjimeneza21@gmail.com>
2024-03-30 02:25:39 -0600

    Inclusion de la licencia a cada uno de los archivos en la documentacion (Tarea 1)
    Signed-off-by: kevin-jim21 <kevinjimeneza21@gmail.com>

--- Tareas/Tarea1/src/Functions.cpp
+++ Tareas/Tarea1/src/Functions.cpp
@@ -1,5 +1,29 @@
-/* En este archivo se definen cada una de las funciones que fueron declaradas
-en el headerfile para utilizar en el juego Ahorcado.*/
+/**
+ * @file Functions.cpp
+ * @brief En este archivo se definen las funciones que fueron declaradas en el headerfile para el juego Ahorcado.
+ *
+ * MIT License
+ *
+ * Copyright (c) [2024] [Kevin Jiménez Acuña]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /*! \mainpage Documentación del Juego Ahorcado
  *
