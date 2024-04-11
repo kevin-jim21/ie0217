0bd9b25d909e8a6582e40332e8e59ee66a709abd
Kevin Jiménez <kevinjimeneza21@gmail.com>
2024-04-11 11:59:03 -0600

    Primer código previo 6.
    Signed-off-by: Kevin Jiménez <kevinjimeneza21@gmail.com>

---
+++ Previos/Previo6/BubbleSort.cpp
@@ -0,0 +1,46 @@
+// Ejemplo algoritmo de ordenamiento Bubble Sort ppt diap 4
+
+#include <iostream>
+
+using namespace std;
+
+// Funcion para aplicar el algoritmo sobre un array
+void bubbleSort(int array[], int size) {
+
+    // Loop para acceder a cada elemento del archivo
+    for (int step = 0; step < size; ++step) {
+
+        // Loop para comparar los elementos adyacentes
+        for (int i = 0; i < size - step; ++i) {
+
+            if (array[i] > array[i + 1]) {
+                
+                // Si detecta que el elemento de la derecha es menor que el actual, los intercambia
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
+// Funcion para imprimir el array
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << "  " << array[i];
+    }
+    cout << "\n";
+}
+
+int main() {
+    int data[] = {-2, 45, 0, 11, -9};
+
+    // Tomar el tamano del array
+    int size = sizeof(data) / sizeof(data[0]);
+
+    // Llamar a la funcion para ordenar el array
+    bubbleSort(data, size);
+
+    cout << "Sorted Array in Ascending Orden:\n";
+    printArray(data, size);
+}
---
+++ Previos/Previo6/README.md
@@ -0,0 +1,6 @@
+# Comandos de compilación para los archivos de código fuente
+
+*Nota: Como es posible notar por el uso del backslash, estos comandos de compilación ejecución fueron utilizados en Windows. Además, el orden en el que se muestran los comandos corresponde al orden en el que se vieron los códigos en la sesion 6.*
+
+* g++ -o .\BubbleSort.exe .\BubbleSort.cpp
+* .\BubbleSort.exe
