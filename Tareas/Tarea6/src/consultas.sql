0c3a6b5a98e26ba52c2085a90aa2df5dc2a17935
Kevin Jiménez <kevinjimeneza21@gmail.com>
2024-06-19 00:14:29 -0600

    Progreso consultas (Tarea 6)
    Signed-off-by: Kevin Jiménez <kevinjimeneza21@gmail.com>

--- Tareas/Tarea6/src/consultas.sql
+++ Tareas/Tarea6/src/consultas.sql
@@ -5,3 +5,13 @@ Workbench para realizar cada una de las consultas solicitadas a la base de
 datos plan_de_estudios
 */
 
+USE plan_de_estudios;
+
+-- Utilizar un join para listar siglas, nombre, semestre, créditos, descripción y dificultad
+SELECT cursos.Sigla, cursos.Nombre, cursos.Semestre, cursos.Creditos, descripciones.Descripcion, descripciones.Dificultad
+FROM cursos
+JOIN descripciones
+ON cursos.CursoID = descripciones.CursoID
+WHERE cursos.Semestre = 'IX' OR cursos.Semestre = 'X';  -- Sólo mostrar cursos de licenciatura
+
+-- Consulta para listar los requisitos de un curso en específico
